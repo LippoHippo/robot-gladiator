88ccc72e7e57e297512bdeda5b78f303eec13e76
Abyers <anthonyw.byers@gmail.com>
2021-12-10 03:36:18 -0600

    console.log intro

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,13 @@
-window.alert("This is an alert! JavaScript is running!");
+var playerName = ("Tony the Robot");
+var name = "your name";
+console.log(name);
+console.log(playerName);
+console.log("This logs a string, good for leaving yourself a messge");
+// this will do math and log 20
+console.log(10 + 10);
+//what is this?
+console.log("Our robot's name is " + playerName);
+function fight() {
+    window.alert("The fight has begun!");
+}
+//fight();
